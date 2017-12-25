08d3aadeeda5fa07d40c758b615393a98abfc7ad
Shin D M <realswoosh@gmail.com>
2017-12-25 14:03:09 +0900

    Excel Loader DLL initialize

---
+++ ExcelLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dmExcelLoader
+{
+	public class ExcelLoader : Loader, ILoader
+	{
+
+		public void Load(string path)
+		{
+			
+		}
+	}
+}
